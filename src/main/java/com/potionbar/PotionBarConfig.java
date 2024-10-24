40b5ad26d1772cc37c2addb8e08a08472531e201
Tyler <tyler-martignetti@hotmail.co.uk>
2024-10-24 01:13:39 +0100

    Removed "example" stuff

--- src/main/java/com/potionbar/PotionBarConfig.java
+++ src/main/java/com/potionbar/PotionBarConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("potionStorageBars")
 public interface PotionBarConfig extends Config
 {
 	@ConfigItem(
